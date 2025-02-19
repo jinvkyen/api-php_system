70558b03a9d383441a42ae73cba55822289e549e
jinvkyen <164635839+jinvkyen@users.noreply.github.com>
2025-02-19 09:10:48 +0800

    Update db.inc.php

--- retailer/retailer/customer/includes/db.inc.php
+++ retailer/retailer/customer/includes/db.inc.php
@@ -2,7 +2,7 @@
 
 	$server_name = "localhost";
 	$db_username = "root";
-	$db_password = "jinSQL";
+	$db_password = "";
 	$db_name = "dbRetailer";
 	
 	$conn = mysqli_connect ($server_name, $db_username, $db_password, $db_name);
